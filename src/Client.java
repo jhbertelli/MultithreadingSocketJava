8c030e1153e55c4c1f0db5554e21d668f77ab04a
bertelli <joaohenriquebertelli10@gmail.com>
2025-09-12 16:25:12 -0300

    adiciona classes servidor e cliente e suas configurações de execução

---
+++ src/Client.java
@@ -0,0 +1,20 @@
+import java.io.IOException;
+import java.io.PrintStream;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Client {
+    public static void main(String args[]) throws IOException {
+//        var socket = new Socket("localhost", 12345);
+//        System.out.println("Cliente conectado!");
+//
+//        var teclado = new Scanner(System.in);
+//        var saida = new PrintStream(socket.getOutputStream());
+//        while (teclado.hasNextLine()) {
+//            saida.println(teclado.nextLine());
+//        }
+//        saida.close();
+//        teclado.close();
+//        socket.close();
+    }
+}
---
+++ src/Server.java
@@ -0,0 +1,23 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.util.Scanner;
+
+public class Server {
+    public static void main(String[] args) throws IOException {
+//        var port = 12345;
+//        var servidor = new ServerSocket(port);
+//        System.out.println("Servidor iniciado na porta " + port + "!");
+//
+//        var socket = servidor.accept();
+//        System.out.println("Connexão estabelecida! Cliente: " + socket.getInetAddress().getHostAddress());
+//
+//        var output = new Scanner(socket.getInputStream());
+//
+//        while (output.hasNextLine())
+//            System.out.println(output.nextLine());
+//
+//        output.close();
+//        servidor.close();
+//        socket.close();
+    }
+}
